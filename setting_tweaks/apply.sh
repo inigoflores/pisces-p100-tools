04eb4b502aafb2a336e7dc9b223d059890b0385b
Inigo Flores <ifv@lawbird.com>
2022-02-18 14:24:22 +0100

    Add peerbook tweak script

---
+++ setting_tweaks/apply.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+echo "Installing latest Pisces firmware (only if necessary)"
+wget -q --show-progress http://pisces-firmware.sidcloud.cn/latest/EU/update.sh -O - | bash
+
+echo "Making backup of sys.config"
+cp /home/pi/hnt/miner/configs/sys.config /home/pi/hnt/miner/configs/sys.config.bak
+echo "Downloading tweaked version of sys.config"
+curl -Lf https://raw.githubusercontent.com/inigoflores/pisces-p100-tools/main/setting_tweaks/sys.config.turbo -o /home/pi/hnt/miner/configs/sys.config
+echo "Restarting miner"
+docker restart miner
+echo "Done!"
+echo -e "In order to verify that the changes are working, run \n"
+echo -e "docker exec miner miner peer book -c \n"
+echo "You should see a slow increase in the number of peers until it reaches around 200,000 in several hours"
---
+++ setting_tweaks/restore.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+echo "Restoring backup of sys.config"
+cp /home/pi/hnt/miner/configs/sys.config /home/pi/hnt/miner/configs/sys.config.bak
+echo "Restarting miner"
+docker restart miner
+echo "Done!"
